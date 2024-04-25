7e4b77b7450ba73df3ca2df4fbf2a1e4d71f7ac7
gregi12 <114067438+gregi12@users.noreply.github.com>
2024-04-25 12:12:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "You're home now! MAMYTO_IMAGE!!"
+    return "You're home now! DEVOPSIORY_IMAGE!!"
 @app.route('/hello-world')
 def hello_world():
     return 'Hello World'
